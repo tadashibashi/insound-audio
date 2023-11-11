f0c72f6b990c47816d2dfbe028dce9661338875e
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-11-11 01:10:39 -0800

    Add security headers to dev server to emulate production environment

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,22 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: []
+  plugins: [],
+  server: {
+    headers: {
+      "content-security-policy": "base-uri 'self'; font-src 'self'; form-action 'self'; frame-ancestors 'self'; img-src 'self'; object-src 'none'; script-src 'self' 'wasm-unsafe-eval' blob:; style-src 'self' 'unsafe-inline'",
+      "cross-origin-embedder-policy": "require-corp",
+      "cross-origin-opener-policy": "same-site",
+      "cross-origin-resource-policy": "same-site",
+      "cross-agent-cluster": "?1",
+      "referrer-policy": "no-referrer",
+      "x-content-type-options": "no-sniff",
+      "x-dns-prefetch-control": "off",
+      "x-download-options": "noopen",
+      "x-frame-options": "SAMEORIGIN",
+      "x-permitted-cross-domain-policies": "none",
+      "x-xss-protection": "0",
+      "Access-Control-Allow-Origin": "http://localhost:5173",
+    }
+  }
 });
