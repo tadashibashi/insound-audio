4de2c458ccba72a9ef4888ee56befd4c54b2ae2f
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-11-12 07:38:26 -0800

    Implement MultiTrackAudio load/unload functions

--- src/cpp/MultiTrackAudio.cpp
+++ src/cpp/MultiTrackAudio.cpp
@@ -1,10 +1,74 @@
 #include "MultiTrackAudio.h"
+#include "common.h"
+
 #include <fmod.hpp>
+#include <cstdlib>
 #include <vector>
 
 namespace Insound {
     struct MultiTrackAudio::Impl {
+    public:
+        Impl(std::string_view name, FMOD::System *sys) : group(), fsb()
+        {
+            FMOD::SoundGroup *group;
+            checkResult(sys->createSoundGroup(name.data(), &group));
+            this->group = group;
+        }
+
+        ~Impl()
+        {
+            group->release();
+        }
+
         FMOD::SoundGroup *group;
-        std::vector<FMOD::Sound *> sounds;
+        FMOD::Sound *fsb;
+
     };
+
+    void MultiTrackAudio::unloadFsb()
+    {
+        if (!m->fsb) return;
+
+        int count;
+        checkResult( m->fsb->getNumSubSounds(&count) );
+
+        for (int i = 0; i < count; ++i)
+        {
+            FMOD::Sound *subsound;
+            checkResult( m->fsb->getSubSound(i, &subsound) );
+            subsound->setSoundGroup(nullptr);
+        }
+
+        m->fsb->release();
+        m->fsb = nullptr;
+    }
+
+    void MultiTrackAudio::loadFsb(const char *data, size_t bytelength)
+    {
+        // Unload any previously loaded fsbank file
+        unloadFsb();
+
+        auto exinfo = FMOD_CREATESOUNDEXINFO();
+        std::memset(&exinfo, 0, sizeof(FMOD_CREATESOUNDEXINFO));
+        exinfo.cbsize = sizeof(FMOD_CREATESOUNDEXINFO);
+        exinfo.length = bytelength;
+
+        FMOD::System *sys;
+        checkResult( m->group->getSystemObject(&sys) );
+
+        FMOD::Sound *snd;
+        checkResult( sys->createSound((const char *)data, FMOD_DEFAULT, &exinfo, &snd) );
+
+        int count;
+        checkResult( snd->getNumSubSounds(&count) );
+
+        for (int i = 0; i < count; ++i)
+        {
+            FMOD::Sound *subsound;
+            checkResult( snd->getSubSound(i, &subsound) );
+            checkResult( subsound->setSoundGroup(m->group) );
+        }
+
+        m->fsb = snd;
+    }
 }
--- src/cpp/MultiTrackAudio.h
+++ src/cpp/MultiTrackAudio.h
@@ -10,14 +10,25 @@ namespace Insound {
     /**
      * Container of loaded audio tracks to be played in sync.
      *
-     * Follows the RAII pattern, where resources are freed when the object is
-     * destroyed.
      */
     class MultiTrackAudio {
     public:
         MultiTrackAudio(std::string_view name, FMOD::System *sys);
         ~MultiTrackAudio();
 
+        /**
+         * Load fsb file from a memory point. Does not copy the memory or own
+         * it. That memory should be stored until no longer needed by the
+         * MultiTrackAudio object.
+         *
+         * @param  data       memory pointer to the fsb
+         * @param  bytelength byte size of the memory block
+         *
+         * @throw FMODError on error
+         */
+        void loadFsb(const char *data, size_t bytelength);
+
+        void unloadFsb();
     private:
         // Pimple idiom
         struct Impl;
