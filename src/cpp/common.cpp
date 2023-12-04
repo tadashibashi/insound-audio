d0d9ee2e90ca4899de285e364ce96b8ab6772066
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-12-04 05:20:53 -0800

    add insensitive case comparison function

--- src/cpp/common.cpp
+++ src/cpp/common.cpp
@@ -1,10 +1,25 @@
 #include "common.h"
 #include <stdexcept>
+#include <string>
 
 namespace Insound {
     void checkResult(FMOD_RESULT result)
     {
         if (result != FMOD_OK)
             throw std::runtime_error(FMOD_ErrorString(result));
+    }
+
+    bool compareCaseInsensitive(std::string_view a, std::string_view b)
+    {
+        const auto size = a.size();
+        if (size != b.size()) return false;
+
+        for (auto aIt = a.begin(), bIt = b.begin(); aIt != a.end(); ++aIt, ++bIt)
+        {
+            if (std::tolower(*aIt) != std::tolower(*bIt))
+                return false;
+        }
+
+        return true;
     }
 }
--- src/cpp/common.h
+++ src/cpp/common.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <fmod_errors.h>
+#include <string_view>
 
 namespace Insound {
     /**
      * Check FMOD result - throw if an error occurred.
      */
     void checkResult(FMOD_RESULT result);
+
+    [[nodiscard]]
+    bool compareCaseInsensitive(std::string_view a, std::string_view b);
 }
