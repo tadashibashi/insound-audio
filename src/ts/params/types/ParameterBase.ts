04304ee4daa66cf26a47d8020f0d774f01dad53a
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-12-07 13:38:59 -0800

    Track if parameters are updated with new value, fire callback only on new value.

--- src/ts/params/types/ParameterBase.ts
+++ src/ts/params/types/ParameterBase.ts
@@ -7,11 +7,13 @@ class ParameterBase
     readonly type: ParamType;
 
     // All parameters have underlying number as value
-    private m_value: number;
+    protected m_value: number;
 
     private onSetCallback: (index: number, value: number) => void;
     readonly defaultValue: number;
     readonly index: number;
+
+    private lastValue: number;
 
     constructor(name: string, index: number, type: ParamType,
         defaultValue: number,
@@ -20,6 +22,7 @@ class ParameterBase
         this.name = name;
         this.type = type;
         this.m_value = defaultValue;
+        this.lastValue = this.m_value;
         this.defaultValue = defaultValue;
         this.onSetCallback = onSetCallback;
         this.index = index;
@@ -28,15 +31,27 @@ class ParameterBase
     get value()
     {
         return this.type === ParamType.Float ? this.m_value :
-            Math.floor(this.m_value);
+            Math.round(this.m_value);
     }
 
     set value(value: number)
     {
         if (this.type === ParamType.Integer)
-            value = Math.floor(value);
-        this.m_value = value;
-        this.onSetCallback(this.index, value);
+            value = Math.round(value);
+
+        const lastValue = this.m_value;
+        this.lastValue = lastValue;
+
+        if (value !== lastValue)
+        {
+            this.m_value = value;
+            this.onSetCallback(this.index, value);
+        }
+    }
+
+    get wasUpdated(): boolean
+    {
+        return this.m_value !== this.lastValue;
     }
 
     transitionTo(value: number, seconds: number)
