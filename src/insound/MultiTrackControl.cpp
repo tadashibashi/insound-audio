1d251a6471a820499c1d4df2c0c4c404ec744e7b
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-01-09 09:32:00 -0800

    Create MultiTrackControl class to interface with frontend JS

---
+++ src/insound/MultiTrackControl.cpp
@@ -0,0 +1,194 @@
+#include "MultiTrackControl.h"
+#include "insound/MultiTrackAudio.h"
+
+namespace Insound
+{
+    MultiTrackControl::MultiTrackControl(MultiTrackAudio *track,
+        emscripten::val callbacks) : lua(), track(track), callbacks(callbacks)
+    {
+    }
+
+    MultiTrackControl::~MultiTrackControl()
+    {
+        delete lua;
+    }
+
+    void MultiTrackControl::loadSound(size_t data, size_t bytelength)
+    {
+        track->loadSound((const char *)data, bytelength);
+    }
+
+    void MultiTrackControl::loadBank(size_t data, size_t bytelength)
+    {
+        track->loadFsb((const char *)data, bytelength);
+    }
+
+    const std::string &MultiTrackControl::loadScript(const std::string &text)
+    {
+       static const std::string NoErrors{};
+
+        auto result = lua->load(text);
+        if (!result)
+            return lua->getError();
+
+        if (!text.empty())
+        {
+            result = lua->doInit();
+            if (!result)
+                return lua->getError();
+
+            result = lua->doLoad(*track);
+            if (!result)
+                return lua->getError();
+        }
+
+        return NoErrors;
+    }
+
+    void MultiTrackControl::unload()
+    {
+        track->clear();
+    }
+
+    bool MultiTrackControl::isLoaded() const
+    {
+        return track->isLoaded();
+    }
+
+    void MultiTrackControl::setPause(bool pause, float seconds)
+    {
+        track->pause(pause, seconds);
+    }
+
+    bool MultiTrackControl::getPause() const
+    {
+        return track->paused();
+    }
+
+    void MultiTrackControl::setVolume(int ch, float volume)
+    {
+        if (ch == 0)
+            track->mainVolume(volume);
+        else
+            track->channelVolume(ch-1, volume);
+    }
+
+    float MultiTrackControl::getVolume(int ch) const
+    {
+        return (ch == 0) ?
+            track->mainVolume() :
+            track->channelVolume(ch-1);
+    }
+
+    void MultiTrackControl::setReverbLevel(int ch, float level)
+    {
+        if (ch == 0)
+            track->mainReverbLevel(level);
+        else
+            track->channelReverbLevel(ch-1, level);
+    }
+
+    float MultiTrackControl::getReverbLevel(int ch) const
+    {
+        return (ch == 0) ?
+            track->mainReverbLevel() :
+            track->channelReverbLevel(ch-1);
+    }
+
+    void MultiTrackControl::setPanLeft(int ch, float level)
+    {
+        if (ch == 0)
+            track->main().panLeft(level);
+        else
+            track->channel(ch-1).panLeft(level);
+    }
+
+    float MultiTrackControl::getPanLeft(int ch) const
+    {
+        return (ch == 0) ?
+            track->main().panLeft() :
+            track->channel(ch-1).panLeft();
+    }
+
+    void MultiTrackControl::setPanRight(int ch, float level)
+    {
+        if (ch == 0)
+            track->main().panRight(level);
+        else
+            track->channel(ch-1).panRight(level);
+    }
+
+    float MultiTrackControl::getPanRight(int ch) const
+    {
+        return (ch == 0) ?
+            track->main().panRight() :
+            track->channel(ch-1).panRight();
+    }
+
+    void MultiTrackControl::setPosition(float seconds)
+    {
+        track->position(seconds);
+    }
+
+    float MultiTrackControl::getPosition() const
+    {
+        return track->position();
+    }
+
+    float MultiTrackControl::getLength() const
+    {
+        return track->length();
+    }
+
+    int MultiTrackControl::getChannelCount() const
+    {
+        return track->channelCount();
+    }
+
+    float MultiTrackControl::getAudibility() const
+    {
+        return track->audibility();
+    }
+
+    void MultiTrackControl::setLooping(bool looping)
+    {
+        track->looping(looping);
+    }
+
+    bool MultiTrackControl::getLooping() const
+    {
+        return track->looping();
+    }
+
+    void MultiTrackControl::setLoopPoint(unsigned loopstart, unsigned loopend)
+    {
+        track->loopMilliseconds(loopstart, loopend);
+    }
+
+    LoopInfo<unsigned> MultiTrackControl::getLoopPoint() const
+    {
+        return track->loopMilliseconds();
+    }
+
+    size_t MultiTrackControl::getSyncPointCount() const
+    {
+        return track->getSyncPointCount();
+    }
+
+    SyncPointInfo MultiTrackControl::getSyncPoint(int index) const
+    {
+        return {
+            .name=track->getSyncPointLabel(index).data(),
+            .offset=track->getSyncPointOffsetMilliseconds(index)
+        };
+    }
+
+    SampleDataInfo MultiTrackControl::getSampleData(int index) const
+    {
+        auto &data = track->getSampleData(index);
+        return {
+            .ptr=(uintptr_t)data.data(),
+            .byteLength=data.size()
+        };
+    }
+}
---
+++ src/insound/MultiTrackControl.h
@@ -0,0 +1,152 @@
+#pragma once
+
+#include <insound/SampleDataInfo.h>
+#include <insound/SyncPointInfo.h>
+#include <insound/LoopInfo.h>
+#include <insound/scripting/LuaDriver.h>
+
+#include <emscripten/val.h>
+
+#include <cstddef>
+#include <string>
+
+namespace Insound
+{
+    class LuaDriver;
+    class MultiTrackAudio;
+
+    /**
+     * Class intended to be bound via Emscripten Embind for use in JavaScript
+     * frontend code. A simple wrapper around MultiTrackAudio for controlling
+     * the track play state and mix.
+     */
+    class MultiTrackControl
+    {
+    public:
+        /**
+         * Callbacks:
+         *     - syncpoint callback (fires syncpoint as it happens to JS)
+         */
+        MultiTrackControl(MultiTrackAudio *track, emscripten::val callbacks);
+
+        ~MultiTrackControl();
+
+        // ----- Loading / Unloading ------------------------------------------
+        void loadSound(size_t data, size_t bytelength);
+        void loadBank(size_t data, size_t bytelength);
+
+        /**
+         * Load lua script
+         *
+         * @param  text - script text to load
+         * @return        error string or empty string if successful load
+         */
+        const std::string &loadScript(const std::string &text);
+
+        /** Unload any currently loaded sounds, script - resets state, etc. */
+        void unload();
+
+        [[nodiscard]]
+        bool isLoaded() const;
+
+        void setPause(bool pause, float seconds = 0);
+
+        [[nodiscard]]
+        bool getPause() const;
+
+        /**
+         * Set the volume of a specific channel
+         *
+         * @param ch     - channel to affect (0 is main bus, 1-chSize are
+         *               individual channels)
+         * @param volume - level to set, where 0 is off, and 1 is 100%
+         */
+        void setVolume(int ch, float volume);
+
+        /**
+         * Get the volume of a specific channel
+         *
+         * @param ch    channel to get value from (0 is the main bus, 1-chSize
+         *              are the individual sub-channels)
+         */
+        [[nodiscard]]
+        float getVolume(int ch) const;
+
+        void setReverbLevel(int ch, float level);
+
+        [[nodiscard]]
+        float getReverbLevel(int ch) const;
+
+        void setPanLeft(int ch, float level);
+
+        [[nodiscard]]
+        float getPanLeft(int ch) const;
+
+        void setPanRight(int ch, float level);
+
+        [[nodiscard]]
+        float getPanRight(int ch) const;
+
+        /**
+         * Set the playhead position of the track (in seconds)
+         *
+         * @param seconds - number of seconds to seek to in the track
+         */
+        void setPosition(float seconds);
+
+        /**
+         * Get the current position of the playhead in the track (in seconds)
+         */
+        [[nodiscard]]
+        float getPosition() const;
+
+        /**
+         * Get loaded track's length in seconds
+         */
+        [[nodiscard]]
+        float getLength() const;
+
+        /**
+         * Get the number of subchannels in the loaded track (doesn't include
+         * the main bus)
+         */
+        [[nodiscard]]
+        int getChannelCount() const;
+
+        [[nodiscard]]
+        float getAudibility() const;
+
+        void setLooping(bool looping);
+
+        [[nodiscard]]
+        bool getLooping() const;
+
+        /**
+         * Set loop points (in milliseconds)
+         *
+         * @param loopstart - loop start point in milliseconds
+         * @param loopend - loop end point in milliseconds
+         */
+        void setLoopPoint(unsigned loopstart, unsigned loopend);
+
+        /**
+         * Get loop points in milliseconds
+         */
+        [[nodiscard]]
+        LoopInfo<unsigned> getLoopPoint() const;
+
+        [[nodiscard]]
+        size_t getSyncPointCount() const;
+
+        [[nodiscard]]
+        SyncPointInfo getSyncPoint(int index) const;
+
+        [[nodiscard]]
+        SampleDataInfo getSampleData(int index) const;
+
+    private:
+        MultiTrackAudio *track;
+        LuaDriver *lua;
+        emscripten::val callbacks;
+    };
+}
